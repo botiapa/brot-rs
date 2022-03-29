8a670bba29621fe6d1364d2ff6690ebdb08b81a8
Botond Szentesi <20019249+botiapa@users.noreply.github.com>
2022-03-29 21:10:19 +0200

    gui start

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::time::Instant;
 
+mod gui;
 mod mandelbrot;
 
 fn main() {
     let start = Instant::now();
+    gui::run_gui();
     mandelbrot::generate_image();
     println!("Elapsed: {}ms", start.elapsed().as_millis());
 }
-
-
